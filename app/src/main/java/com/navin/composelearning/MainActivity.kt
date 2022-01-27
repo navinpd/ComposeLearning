e5122c74a9aa3e0426b801331d8a19afbc3dd3e6
Navin prasad <navin.pd@gmail.com>
2022-01-27 21:29:57 +0800

    Updated to show password text field

--- app/src/main/java/com/navin/composelearning/MainActivity.kt
+++ app/src/main/java/com/navin/composelearning/MainActivity.kt
@@ -1,31 +1,24 @@
 package com.navin.composelearning
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.navin.composelearning.ui.theme.ComposeLearningTheme
-import com.navin.composelearning.ui.theme.GoogleButton
 
 class MainActivity : ComponentActivity() {
     @ExperimentalMaterialApi
@@ -45,7 +38,34 @@ class MainActivity : ComponentActivity() {
 @ExperimentalMaterialApi
 @Composable
 fun Greeting() {
-    GoogleButton()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var data by remember { mutableStateOf("Data") }
+        var showPassword by rememberSaveable { mutableStateOf(false) }
+        val painter =
+            if (showPassword) R.drawable.design_ic_visibility else R.drawable.design_ic_visibility_off
+
+        OutlinedTextField(
+            value = data,
+            onValueChange = { data = it },
+            trailingIcon = {
+                IconButton(onClick = { showPassword = !showPassword }) {
+                    Icon(
+                        painter = painterResource(id = painter),
+                        contentDescription = "Hide"
+                    )
+                }
+            },
+            label = { Text(text = "Password") },
+            visualTransformation = if (showPassword)
+                VisualTransformation.None else PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            keyboardActions = KeyboardActions(onNext = {})
+        )
+    }
 }
 
 @ExperimentalMaterialApi
