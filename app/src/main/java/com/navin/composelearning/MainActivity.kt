b18bd85183854a3d27f7d4b5a8a681b300584e23
Navin prasad <navin.pd@gmail.com>
2022-01-27 13:17:24 +0800

    Update text field and outlined text field

--- app/src/main/java/com/navin/composelearning/MainActivity.kt
+++ app/src/main/java/com/navin/composelearning/MainActivity.kt
@@ -1,34 +1,24 @@
 package com.navin.composelearning
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.navin.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,10 +29,59 @@ class MainActivity : ComponentActivity() {
             ComposeLearningTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ExpandableCard()
+                    Greeting()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting() {
+    var text by remember { mutableStateOf("Type Here...") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        OutlinedTextField(
+//        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text(text = "Title")
+            },
+            maxLines = 1,
+            leadingIcon = {
+                IconButton(onClick = {}, enabled = true) {
+                    Icon(
+                        imageVector = Icons.Filled.Email,
+                        contentDescription = "TextIcon"
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {
+                    Log.d("TAG", "I'm clicked")
+                }, enabled = true) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "TextIcon"
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = true,
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(onSearch = {},
+                onNext = { Log.d("TAG", "Message") })
+        )
     }
 }
 
@@ -52,7 +91,7 @@ class MainActivity : ComponentActivity() {
 fun DefaultPreview() {
     ComposeLearningTheme {
         Surface(color = MaterialTheme.colors.background) {
-            ExpandableCard()
+            Greeting()
         }
     }
 }
