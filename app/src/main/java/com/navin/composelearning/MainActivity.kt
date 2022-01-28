a0d80b42fd462fa46c09bb129317800606f42752
Navin prasad <navin.pd@gmail.com>
2022-01-28 12:13:33 +0800

    Lazy column and sticky header

--- app/src/main/java/com/navin/composelearning/MainActivity.kt
+++ app/src/main/java/com/navin/composelearning/MainActivity.kt
@@ -3,19 +3,26 @@ package com.navin.composelearning
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.navin.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,12 +31,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting(
-                        gradient = Brush.horizontalGradient(
-                            colors = listOf(
-                                Color.Blue,
-                                Color.DarkGray
-                            )
-                        )
+                        persons
                     )
                 }
             }
@@ -37,35 +39,70 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalFoundationApi
 @ExperimentalMaterialApi
 @Composable
 fun Greeting(
-    gradient: Brush,
-    text: String = "Button"
+    persons: List<Person>
 ) {
+    var sections = listOf("A", "B", "C", "D", "E")
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
-            onClick = {}, contentPadding = PaddingValues(),
-            elevation = ButtonDefaults.elevation(defaultElevation = 6.dp, pressedElevation = 12.dp)
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(all = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Box(
-                modifier = Modifier.background(gradient)
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .width(128.dp)
-                    .height(46.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(text = text, color = Color.White)
+//            itemsIndexed(items = persons) { position, person ->
+//                Log.d("TAG", "Position $position")
+//                AdapterItem(person)
+//            }
+            sections.forEach { section ->
+                stickyHeader {
+                    Text(
+                        text = "Section $section",
+                        modifier = Modifier.fillMaxWidth()
+                            .background(color = Color.LightGray)
+                            .padding(all = 12.dp)
+                    )
+                }
+                items(5) {
+                    Text(text = "Item $it of section $section")
+                }
             }
+        }
+
+    }
+}
+
+@Composable
+fun AdapterItem(person: Person) {
+    Row(
+        modifier = Modifier.fillMaxWidth().height(104.dp)
+            .background(color = Color.Gray),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+
+        Text(
+            text = person.name, color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp
+        )
+        Column(
+            modifier = Modifier.fillMaxHeight().padding(24.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = person.age.toString(), color = Color.Black, fontSize = 22.sp)
+            Text(text = person.sex, color = Color.Blue, fontSize = 22.sp)
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @ExperimentalMaterialApi
 @Preview(showBackground = true)
 @Composable
@@ -73,13 +110,21 @@ fun DefaultPreview() {
     ComposeLearningTheme {
         Surface(color = MaterialTheme.colors.background) {
             Greeting(
-                gradient = Brush.horizontalGradient(
-                    colors = listOf(
-                        Color.Red,
-                        Color.Cyan
-                    )
-                )
+                persons
             )
         }
     }
 }
+
+var persons = listOf<Person>(
+    Person("Navin", 34, "M"),
+    Person("Varsha", 34, "F"),
+    Person("Sanjit", 34, "M"),
+    Person("Sarika", 34, "F"),
+    Person("Varsha", 34, "F"),
+    Person("Sanjit", 34, "M"),
+    Person("Navin", 34, "M"),
+    Person("Varsha", 34, "F"),
+)
+
+data class Person(var name: String, var age: Int, var sex: String)
